a73cb6dd6827e5ff5069d90870f353aafcfff530
Jaco van den Bergh <jacovdbergh@gmail.com>
2021-09-29 14:54:45 +0200

    Stash... not only 1 game per match...

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -15,4 +15,42 @@ class Player extends Model
     {
         return $this->belongsTo(Tournament::class);
     }
+
+    public function player1Games()
+    {
+        return $this->hasMany(Game::class, 'player_1_id', 'id');
+    }
+
+    public function player2Games()
+    {
+        return $this->hasMany(Game::class, 'player_2_id', 'id');
+    }
+
+    public function player1GamesPlayed()
+    {
+        return $this->player1Games()->whereNotNull('player_1_score');
+    }
+
+    public function player2GamesPlayed()
+    {
+        return $this->player2Games()->whereNotNull('player_1_score');
+    }
+
+    public function player1GamesWon()
+    {
+        return $this->player1GamesPlayed()->whereColumn('player_1_score', '>', 'player_2_score');
+    }
+
+    public function player2GamesWon()
+    {
+        return $this->player2GamesPlayed()->whereColumn('player_2_score', '>', 'player_1_score');
+    }
+
+    public function pointDiff()
+    {
+        $this->loadMissing('player1Games', 'player2Games');
+
+        return $this->player1Games->sum('player_1_score') - $this->player1Games->sum('player_2_score')
+            + $this->player2Games->sum('player_2_score') - $this->player2Games->sum('player_1_score');
+    }
 }
--- resources/views/livewire/tournaments/show.blade.php
+++ resources/views/livewire/tournaments/show.blade.php
@@ -1,14 +1,95 @@
 <div>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-cyan-900 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-cyan-900">
             {{ $tournament->name }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="py-10 space-y-8">
+        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+            <div class="flex flex-col">
+                <div class="overflow-x-auto">
+                    <div class="inline-block min-w-full align-middle">
+                        <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >
+                                            Player
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >
+                                            P
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >
+                                            W
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >
+                                            L
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                                        >
+                                            PD
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            class="hidden px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase sm:block"
+                                        >
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($tournament->players()->withCount('player1GamesPlayed', 'player2GamesPlayed', 'player1GamesWon', 'player2GamesWon')
+                                        ->get()->sortBy([
+                                            fn($a, $b) => $b->player1_games_won_count + $b->player2_games_won_count <=> $a->player1_games_won_count + $a->player2_games_won_count,
+                                            fn($a, $b) => $b->player1_games_played_count + $b->player2_games_played_count <=> $a->player1_games_played_count + $a->player2_games_played_count,
+                                            fn($a, $b) => $b->pointDiff() <=> $a->pointDiff(),
+                                        ]) as $player)
+                                        <tr>
+                                            <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                {{ $player->name }}
+                                            </td>
+                                            <td class="w-16 px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                                {{ $player->player1_games_played_count + $player->player2_games_played_count }}
+                                            </td>
+                                            <td class="w-16 px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                                {{ $player->player1_games_won_count + $player->player2_games_won_count }}
+                                            </td>
+                                            <td class="w-16 px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                                {{ ($player->player1_games_played_count + $player->player2_games_played_count)
+                                                 - ($player->player1_games_won_count + $player->player2_games_won_count) }}
+                                            </td>
+                                            <td class="w-16 px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                                {{ $player->pointDiff() }}
+                                            </td>
+                                            <td
+                                                class="hidden px-6 py-4 text-sm text-gray-500 sm:block whitespace-nowrap">
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
             <div class="p-6 space-y-6">
-                @foreach ($tournament->games->groupBy('round') as $round)
+                @foreach ($tournament->games()->with('player1', 'player2')->get()->groupBy('round') as $round)
                     <div class="">
                         <span class="font-bold">Round {{ $loop->iteration }}</span>
                         @foreach ($round as $game)
@@ -22,10 +103,6 @@
                 @endforeach
             </div>
         </div>
-        <div class="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6">
-                Show leaderboard
-            </div>
-        </div>
     </div>
+</div>
 </div>
