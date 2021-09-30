60fc344b0e8b27c85f3039f9578658b1a9ec2975
Jaco van den Bergh <jacovdbergh@gmail.com>
2021-09-30 21:22:46 +0200

    Deebugged

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,8 @@
             <a href="/">
                 <x-application-logo class="w-24 h-24" />
             </a>
-            <div class="mt-10 flex flex-col items-center">
-                <h1 class="font-bold text-2xl text-orange-600">Register</h1>
+            <div class="flex flex-col items-center mt-10">
+                <h1 class="text-2xl font-bold text-orange-600">Register</h1>
             </div>
         </x-slot>
 
@@ -19,21 +19,21 @@
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="block w-full mt-1"
                                 type="password"
                                 name="password"
                                 required autocomplete="new-password" />
@@ -43,18 +43,18 @@
             <div class="mt-4">
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
+                <x-input id="password_confirmation" class="block w-full mt-1"
                                 type="password"
                                 name="password_confirmation" required 
                                 autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="text-sm text-gray-600 underline hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-button class="ml-4" type="submit>
+                <x-button class="ml-4" type="submit">
                     {{ __('Register') }}
                 </x-button>
             </div>
