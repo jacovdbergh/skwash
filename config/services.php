8db734bab3062f52b973df18984929535fc40eb6
Jaco van den Bergh <jacovdbergh@gmail.com>
2021-10-01 09:00:03 +0200

    Nou nou

--- config/services.php
+++ config/services.php
@@ -27,6 +27,7 @@ return [
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'token' => env('AWS_SESSION_TOKEN'),
         'region' => 'eu-west-2',
     ],
 
